2b471f8e29f06a5c6193333f761140395dfd25b8
Anton Mamakin <antonmamakinscript@gmail.com>
2021-05-25 19:45:16 +0400

    start

---
+++ src/test/java/com/example/cpserver/CpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
