c3b37d8ed6990e88ae6564dfc701e68c398be136
Anton Mamakin <antonmamakinscript@gmail.com>
2021-05-29 23:03:31 +0400

    hot fix

--- src/main/java/com/example/cpserver/theme/controller/ThemeController.java
+++ src/main/java/com/example/cpserver/theme/controller/ThemeController.java
@@ -3,6 +3,7 @@ package com.example.cpserver.theme.controller;
 import com.example.cpserver.general.dto.Message;
 import com.example.cpserver.theme.controller.dto.CreateThemeDto;
 import com.example.cpserver.theme.model.Theme;
+import com.example.cpserver.theme.repo.ThemeRepo;
 import com.example.cpserver.theme.service.ThemeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,10 +15,17 @@ import java.util.List;
 public class ThemeController {
 
     private final ThemeService themeService;
+    private final ThemeRepo themeRepo;
 
     @Autowired
-    public ThemeController(ThemeService themeService) {
+    public ThemeController(ThemeService themeService, ThemeRepo themeRepo) {
         this.themeService = themeService;
+        this.themeRepo = themeRepo;
+    }
+
+    @GetMapping
+    public Theme getTheme(@RequestParam Long id){
+        return themeRepo.findById(id).get();
     }
 
 
